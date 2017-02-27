cdc97a41a93d793bf2c47d93f92a76907e972a75
Reyth3 <mrreynevan2@gmail.com>
2017-02-27 11:33:07 +0100

    Initialized Project & Set up Entity Framework

---
+++ CSharpCExam/Models/DB/Entry.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CSharpCExam.Models.DB
+{
+    public class Entry
+    {
+        [Key]
+        public int Id { get; set; }
+        [MaxLength(256)]
+        public string Nickname { get; set; }
+        [MaxLength(256)]
+        public string Bio { get; set; }
+        public int Answer1 { get; set; }
+        public int Answer2 { get; set; }
+        public int Answer3 { get; set; }
+        public int Answer4 { get; set; }
+    }
+}
